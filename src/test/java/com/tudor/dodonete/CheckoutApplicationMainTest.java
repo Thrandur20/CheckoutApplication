4f191e744c9a1eb719161fdfc4ebc6c4ec1da4b3
Tudor Dodonete <tudor.dodonete@gmail.com>
2021-07-18 17:54:47 +0100

    added main and test

--- src/test/java/com/tudor/dodonete/CheckoutApplicationMainTest.java
+++ src/test/java/com/tudor/dodonete/CheckoutApplicationMainTest.java
@@ -1,9 +1,42 @@
 package com.tudor.dodonete;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.AbstractMap;
+import java.util.Map;
+
+import static org.junit.Assert.fail;
 
 class CheckoutApplicationMainTest {
 
+    @Test
+    public void testDataInsert() throws Exception {
+        Map<String, Double> enterValue = Map.of("A", 0.5);
+        String input = "A,0.5";
+        InputStream sysBack = System.in;
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertEquals(enterValue, Basket.addProducts(in));
+        System.setIn(sysBack);
+    }
 
+    @Test
+    public void testDataInsertButFailFormat() throws Exception {
+        String input = "A";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        fail(Basket.addProducts());
+    }
+
+    private Map<String, Double> productList() {
+        return Map.ofEntries(
+                new AbstractMap.SimpleEntry<>("A", 0.5),
+                new AbstractMap.SimpleEntry<>("B", 0.6),
+                new AbstractMap.SimpleEntry<>("C", 0.25),
+                new AbstractMap.SimpleEntry<>("D", 1.5),
+                new AbstractMap.SimpleEntry<>("E", 2.0)
+        );
+    }
 
 }
